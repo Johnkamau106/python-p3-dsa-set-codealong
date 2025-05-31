9871fe0d6960e8c44dfd06701c11e1330c5dd06d
Your Name <johnkamau106@gmail.com>
2025-06-01 00:49:01 +0300

    Implement MySet class with add, delete, has, and size methods

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, elements=None):
+        self.dictionary = {}
+        if elements:
+            for element in elements:
+                self.dictionary[element] = True
 
-    pass
+    def add(self, element):
+        self.dictionary[element] = True
+
+    def delete(self, element):
+        if element in self.dictionary:
+            del self.dictionary[element]
+
+    def has(self, element):
+        return element in self.dictionary
+
+    def size(self):
+        return len(self.dictionary)
